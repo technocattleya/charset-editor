6cb5dda0503e9d93bae7410132cb9085fec61cdd
wsagami <sagami.wataru@technocattleya.com>
2017-06-11 23:30:16 +0900

    Add support for string to hex code conversion

--- src/raw/js/app.js
+++ src/raw/js/app.js
@@ -17,7 +17,25 @@ var vm = new Vue({
       this.input = e.target.value
     }, 300),
     stringToCodeString: function () {
-      return this.input;
+      var utf8str, codeDecimal, codeString;
+      var string = this.input;
+      var type = "16";
+      var digit = 2;
+
+      utf8str = Encoding.convert(string, 'UTF8');
+      codeDecimal = Encoding.stringToCode(utf8str);
+
+      if (type !== "url") {
+        codeString = codeDecimal
+          .map((decimal) => (
+            Array(digit + 1).join("0") + decimal.toString(type)
+          ).slice(-digit))
+          .join("");
+      } else {
+        codeString = Encoding.urlEncode(codeDecimal);
+      }
+
+      return codeString;
     }
   }
 });
